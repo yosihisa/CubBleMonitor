d24e9ea001705169616e5c16dd469ce56e7bf516
よしひさ <goto.teru652@mail.kyutech.jp>
2021-02-07 22:08:52 +0900

    時計追加　UI調整

--- docs/BLE.js
+++ docs/BLE.js
@@ -41,24 +41,31 @@ ble.onRead = function (data, uuid) {
     //フォーマットに従って値を取得
     rpm   = data.getInt16(0);
     shift = data.getInt8(2);
-    temp  = data.getInt8(3);
-    batt  = data.getInt8(4);
+    temp =  data.getUint8(3);
+    batt  = data.getUint8(4);
     speed = data.getInt16(5);
     mode  = data.getInt8(7);
 
     rpm = rpm * 10;
     batt = batt / 10;
+    temp = temp - 20;
     speedH = speed / 10;
     speedL = speed % 10;
     if (shift == 0) shift = 'N';
     if (mode == 78) mode = "N";
     if (mode == 65) mode = "A";
+    if (mode == 68) mode = "D";
+    if (mode == 69) mode = "E";
+
+
+    var nowTime = new Date();
+    var Hour = nowTime.getHours();
+    var Min = nowTime.getMinutes();
 
     //コンソールに値を表示
     //console.log(value);
 
     //HTMLにデータを表示
-
     document.getElementById('data_rpm').innerHTML = rpm;
     document.getElementById('data_shift').innerHTML = shift;
     document.getElementById('data_temp').innerHTML = temp;
@@ -67,9 +74,29 @@ ble.onRead = function (data, uuid) {
     document.getElementById('data_speedL').innerHTML = '.' + speedL;
     document.getElementById('data_mode').innerHTML = mode;
 
+    document.getElementById("time").innerHTML = ("00" + Hour).slice(-2) +`:`+ ("00" + Min).slice(-2) ;
 
     document.getElementById('uuid_name').innerHTML = uuid;
     document.getElementById('status').innerHTML = "read data"
+
+    //色の変更
+    if (mode == "N")document.getElementById('data_mode').style.color = "#F00";
+    if (mode == "A")document.getElementById('data_mode').style.color = "#0F0";
+    if (mode == "D")document.getElementById('data_mode').style.color = "#0F0";
+    if (mode == "E")document.getElementById('data_mode').style.color = "#FF0";
+    if (batt <= 10) document.getElementById('data_batt').style.color = "#FF0";
+    else            document.getElementById('data_batt').style.color = "#FFF";
+    if (temp <= 55) document.getElementById('data_temp').style.color = "#FF0";
+    else            document.getElementById('data_temp').style.color = "#FFF";
+
+    if (shift == `N`) document.getElementById('data_shift').style.color = "#0F0";
+    else              document.getElementById('data_shift').style.color = "#FFF";
+    
+    if (rpm < 5500) document.getElementById('data_rpm').style.color = "#FFF";
+    else if (rpm < 7500) document.getElementById('data_rpm').style.color = "#0FF";
+    else if (rpm < 8400) document.getElementById('data_rpm').style.color = "#FF0";
+    else document.getElementById('data_rpm').style.color = "#F00";
+
 }
 
 
