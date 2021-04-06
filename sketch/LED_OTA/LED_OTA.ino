d0f54b6cac4a1fb3e975313ef7d717b3c42457f2
よしひさ <goto.teru652@mail.kyutech.jp>
2021-04-06 20:23:08 +0900

    Update LED_OTA.ino

--- sketch/LED_OTA/LED_OTA.ino
+++ sketch/LED_OTA/LED_OTA.ino
@@ -18,9 +18,6 @@ const char *password = "ESP32ESP32";
 #define LED_G_CHANNEL 1
 #define LED_B_CHANNEL 2
 
-bool ota_mode = false;
-int cnt;
-
 void setup()
 {
     delay(1000);
@@ -43,58 +40,53 @@ void setup()
     Serial.println("init WiFi");
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() == WL_CONNECTED)
-    {
-        ota_mode = true;
+    if (WiFi.waitForConnectResult() == WL_CONNECTED) {
         Serial.println("WiFi Connected");
         Serial.print("IP address: ");
         Serial.println(WiFi.localIP());
     }else{
-        ota_mode = false;
         Serial.println("Connection Failed! Rebooting...");
         delay(5000);
         ESP.restart();
     }
 
-    if (ota_mode ==true){
-        Serial.println("init OTA");
+    Serial.println("init OTA");
 
-        ArduinoOTA.setHostname("CubBleMonitor");
-        ArduinoOTA
-            .onStart([]() {
-                String type;
-                if (ArduinoOTA.getCommand() == U_FLASH)
-                    type = "sketch";
-                else // U_SPIFFS
-                    type = "filesystem";
+    ArduinoOTA.setHostname("CubBleMonitor");
+    ArduinoOTA
+        .onStart([]() {
+            String type;
+            if (ArduinoOTA.getCommand() == U_FLASH)
+                type = "sketch";
+            else // U_SPIFFS
+                type = "filesystem";
 
-                // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-                Serial.println("Start updating " + type);
-            })
-            .onEnd([]() {
-                Serial.println("\nEnd");
-            })
-            .onProgress([](unsigned int progress, unsigned int total) {
-                Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-            })
-            .onError([](ota_error_t error) {
-                Serial.printf("Error[%u]: ", error);
-                if (error == OTA_AUTH_ERROR)
-                    Serial.println("Auth Failed");
-                else if (error == OTA_BEGIN_ERROR)
-                    Serial.println("Begin Failed");
-                else if (error == OTA_CONNECT_ERROR)
-                    Serial.println("Connect Failed");
-                else if (error == OTA_RECEIVE_ERROR)
-                    Serial.println("Receive Failed");
-                else if (error == OTA_END_ERROR)
-                    Serial.println("End Failed");
-            });
+            // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+            Serial.println("Start updating " + type);
+        })
+        .onEnd([]() {
+            Serial.println("\nEnd");
+        })
+        .onProgress([](unsigned int progress, unsigned int total) {
+            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        })
+        .onError([](ota_error_t error) {
+            Serial.printf("Error[%u]: ", error);
+            if (error == OTA_AUTH_ERROR)
+                Serial.println("Auth Failed");
+            else if (error == OTA_BEGIN_ERROR)
+                Serial.println("Begin Failed");
+            else if (error == OTA_CONNECT_ERROR)
+                Serial.println("Connect Failed");
+            else if (error == OTA_RECEIVE_ERROR)
+                Serial.println("Receive Failed");
+            else if (error == OTA_END_ERROR)
+                Serial.println("End Failed");
+        });
 
-        Serial.println("Begin OTA");
-        ArduinoOTA.begin();
-        Serial.println("OTA Ready");
-    }
+    Serial.println("Begin OTA");
+    ArduinoOTA.begin();
+    Serial.println("OTA Ready");
 
     cnt = 0;
 }
@@ -102,25 +94,20 @@ void setup()
 
 void loop()
 {
-    if(ota_mode == true){
-        ArduinoOTA.handle();
-    }
+    ArduinoOTA.handle();
 
-    if ((cnt / 256) % 3 == 0)
-    {
+    if ((cnt / 256) % 3 == 0) {
         ledcWrite(LED_R_CHANNEL, cnt % 255);
         ledcWrite(LED_G_CHANNEL, 0);
         ledcWrite(LED_B_CHANNEL, 0);
     }
 
-    if ((cnt / 256) % 3 == 1)
-    {
+    if ((cnt / 256) % 3 == 1) {
         ledcWrite(LED_R_CHANNEL, 0);
         ledcWrite(LED_G_CHANNEL, cnt % 255);
         ledcWrite(LED_B_CHANNEL, 0);
     }
-    if ((cnt / 256) % 3 == 2)
-    {
+    if ((cnt / 256) % 3 == 2) {
         ledcWrite(LED_R_CHANNEL, 0);
         ledcWrite(LED_G_CHANNEL, 0);
         ledcWrite(LED_B_CHANNEL, cnt % 255);
