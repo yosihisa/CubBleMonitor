62e180b31e94f26f4b8a31b55fcedc38f44a4e86
よしひさ <goto.teru652@mail.kyutech.jp>
2021-04-10 02:42:02 +0900

    メーターレイアウト更新

--- docs/BLE.js
+++ docs/BLE.js
@@ -1,16 +1,3 @@
-//時計
-setInterval('showClock1()', 1000);
-
-function showClock1() {
-    var nowTime = new Date();
-    var nowHour = nowTime.getHours();
-    var nowMin = nowTime.getMinutes();
-    document.getElementById("timeH").innerHTML = ("00" + nowHour).slice(-2);
-    document.getElementById("timeM").innerHTML = ("00" + nowMin).slice(-2);
-
-}
-
-var count = 0;
 var connected = 0;
 
 //--------------------------------------------------
@@ -63,83 +50,15 @@ ble.onRead = function (data, uuid) {
     rpm = rpm ;
     batt = batt / 10;
     temp = temp - 20;
-    speedH = speed / 10;
-    speedL = speed % 10;
     if (shift == 0) shift = 'N';
     if (mode == 78) mode = "N";
     if (mode == 65) mode = "A";
     if (mode == 68) mode = "D";
     if (mode == 69) mode = "E";
 
-    count = count + 1;
-
-
-    //HTMLにデータを表示
-    document.getElementById('data_rpm').innerHTML = rpm;
-    document.getElementById('data_shift').innerHTML = shift;
-    document.getElementById('data_temp').innerHTML = temp;
-    document.getElementById('data_batt').innerHTML = batt.toFixed(1);
-    document.getElementById('data_speedH').innerHTML = speedH.toFixed(0);
-    document.getElementById('data_speedL').innerHTML = '.' + speedL;
-    document.getElementById('data_mode').innerHTML = mode;
-
     document.getElementById('uuid_name').innerHTML = uuid;
-    document.getElementById('status').innerHTML = "read data"
 
-    //色の変更
-    if (mode == "N")document.getElementById('data_mode').style.color = "#F00";
-    if (mode == "A")document.getElementById('data_mode').style.color = "#0F0";
-    if (mode == "D")document.getElementById('data_mode').style.color = "#0F0";
-    if (mode == "E")document.getElementById('data_mode').style.color = "#FF0";
-    if (batt <= 10) document.getElementById('data_batt').style.color = "#FF0";
-    else            document.getElementById('data_batt').style.color = "#FFF";
-    if (temp <= 55) document.getElementById('data_temp').style.color = "#FF0";
-    else            document.getElementById('data_temp').style.color = "#FFF";
-
-    if (shift == `N`) document.getElementById('data_shift').style.color = "#0F0";
-    else              document.getElementById('data_shift').style.color = "#FFF";
-    
-    if (rpm < 5500) document.getElementById('data_rpm').style.color = "#FFF";
-    else if (rpm < 7500) document.getElementById('data_rpm').style.color = "#0FF";
-    else if (rpm < 8400) document.getElementById('data_rpm').style.color = "#FF0";
-    else document.getElementById('data_rpm').style.color = "#F00";
-
-    if (count % 10 < 5) document.getElementById('timeS').style.color = "#FFF";
-    else document.getElementById('timeS').style.color = "#000";
-
-    if (speed < 450) {
-        document.getElementById('speed').style.color = "#FFF";
-        document.getElementById('speed').style.backgroundColor = "#111";
-    } else if (speed < 575) {
-        document.getElementById('speed').style.color = "#FFF";
-        document.getElementById('speed').style.backgroundColor = "#02D";
-    } else if (speed < 625) {
-        if (count % 8 < 4) {
-            document.getElementById('speed').style.color = "#FFF";
-            document.getElementById('speed').style.backgroundColor = "#0F2";
-        } else {
-            document.getElementById('speed').style.color = "#FFF";
-            document.getElementById('speed').style.backgroundColor = "#111";
-        }
-    } else if (speed < 725) {
-        if (count % 4 < 2) {
-            document.getElementById('speed').style.color = "#FFF";
-            document.getElementById('speed').style.backgroundColor = "#F80";
-        } else {
-            document.getElementById('speed').style.color = "#FFF";
-            document.getElementById('speed').style.backgroundColor = "#111";
-        }
-    } else {
-        if (count % 2 < 1) {
-            document.getElementById('speed').style.color = "#FFF";
-            document.getElementById('speed').style.backgroundColor = "#F00";
-        } else {
-            document.getElementById('speed').style.color = "#FFF";
-            document.getElementById('speed').style.backgroundColor = "#111";
-        }
-    }
-
-
+    updateParam(rpm, shift, temp, batt, speed, mode);
 }
 
 
@@ -200,14 +119,15 @@ function connectBLE() {
 }
 
 document.getElementById('startNotifications').addEventListener('click', function () {
+
     if (connected == 0) {
         setInterval(connectBLE, 500);
     }
     //ble.startNotify('UUID1');
 });
 
+/*
 document.getElementById('stopNotifications').addEventListener('click', function () {
     ble.stopNotify('UUID1');
 });
-
-
+*/
---
+++ docs/display.js
@@ -0,0 +1,178 @@
+var Rpm ;
+var Shift;
+var Temp;
+var Batt = 0;
+var Speed;
+var Mode;
+
+var count = 0;
+
+var cv;
+var id;
+var canvas
+
+
+updateParam = function (rpm, shift, temp, batt, speed, mode) {
+    Rpm = parseInt(rpm,10);
+    Shift = shift;
+    Temp = temp;
+    Batt = batt;
+    Speed = speed;
+    Mode = mode;
+
+    count = count + 1;
+}
+
+function Start_drawing() {
+    console.log('draw');
+    canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+        cv = canvas.getContext('2d');
+        cv.fillStyle = "rgb(255,255,255)";
+        cv.fillRect(0, 0, 380, 250);
+        cv.fill();
+        id = setInterval(ndraw, 25);
+    }
+
+    Rpm = 0;
+}
+
+function ndraw() {
+    var MAX_RPM = 10000;
+    //背景
+    cv.fillStyle = "rgb(0,0,0)";
+    cv.fillRect(0, 0, 380, 250);
+
+    //タコメーター
+    cv.fillStyle = "#222";
+    cv.fillRect(0, 0, 380, 200);
+    cv.fillStyle = "#080";
+    cv.beginPath();
+    cv.arc(190, 200, 300, -Math.PI, (-1+(Rpm / MAX_RPM)) * (3.1415), false);
+    cv.lineTo(190, 200);
+    cv.closePath();
+    cv.fill();
+
+    if (Rpm > 2300) {
+        cv.fillStyle = "#0F0";
+        cv.beginPath();
+        cv.arc(190, 200, 300, (-1 + (2300 / MAX_RPM)) * (3.1415), (-1 + (Rpm / MAX_RPM)) * (3.1415), false);
+        cv.lineTo(190, 200);
+        cv.closePath();
+        cv.fill();
+    }
+
+    if (Rpm > 5300) {
+        cv.fillStyle = "#FF0";
+        cv.beginPath();
+        cv.arc(190, 200, 300, (-1 + (5300 / MAX_RPM)) * (3.1415), (-1 + (Rpm / MAX_RPM)) * (3.1415), false);
+        cv.lineTo(190, 200);
+        cv.closePath();
+        cv.fill();
+    }
+
+    if (Rpm > 8400) {
+        cv.fillStyle = "#F00";
+        cv.beginPath();
+        cv.arc(190, 200, 300, (-1 + (8400 / MAX_RPM)) * (3.1415), (-1 + (Rpm / MAX_RPM)) * (3.1415), false);
+        cv.lineTo(190, 200);
+        cv.closePath();
+        cv.fill();
+    }
+
+    //タコメーターカバー
+    cv.fillStyle = "#111"
+    cv.beginPath();
+    cv.fillRect(20, 20, 340, 180);
+    cv.closePath();
+
+
+    //速度
+    speedH = (Speed / 10).toFixed(0);
+    speedL = Speed % 10;
+    cv.fillStyle = "#FFF"
+    cv.font = 'bold 120pt Oswald';
+    cv.textAlign ='end'
+    cv.fillText(speedH, 298, 150,180);
+
+    cv.font = 'bold 60pt Oswald';
+    cv.textAlign = 'start'
+    cv.fillText(speedL, 305, 150, 50);
+
+    cv.font = '30pt Oswald';
+    cv.textAlign = 'start'
+    cv.fillText('km/h', 305, 70, 50);
+
+    //シフトインジケータ
+    cv.fillStyle = "#0FF"
+    if (Shift == `N`) {
+        cv.fillStyle = "#0F0"
+    }
+    cv.font = 'bold 70pt Oswald';
+    cv.textAlign = 'start'
+    cv.fillText(Shift, 40, 100, 50);
+
+    //時計
+    var nowTime = new Date();
+    var nowHour = nowTime.getHours();
+    var nowMin = nowTime.getMinutes();
+    cv.fillStyle = "#FFF"
+    cv.font = 'bold 40pt Oswald';
+    cv.textAlign = 'start'    
+    cv.fillText(("00" + nowHour).slice(-2) + ':' + ("00" + nowMin).slice(-2), 30, 150, 80);
+
+    //温度
+    cv.fillStyle = "#FFF"
+    cv.font = '20pt Oswald';
+    cv.textAlign = 'start'
+    cv.fillText(Temp+'℃', 40, 190, 60);
+
+    //バッテリ電圧
+    cv.fillStyle = "#FFF"
+    cv.font = '20pt Oswald';
+    cv.textAlign = 'end'
+    cv.fillText(Batt.toFixed(1) + 'V', 190, 190, 80);
+
+    cv.fillStyle = "#333";
+    cv.beginPath();
+    cv.fillRect(200, 170, 50, 20);
+    cv.closePath();
+    cv.fillStyle = "#080";
+    if (Batt < 12) {
+        cv.fillStyle = "#FF0";
+    }
+    cv.beginPath();
+    cv.fillRect(200, 170, 50*(Batt/16), 20);
+    cv.closePath();
+
+    //GNSS
+    cv.fillStyle = "#FFF"
+    cv.font = '20pt Oswald';
+    cv.textAlign = 'start'
+    cv.fillText('GNSS', 275, 190, 50);
+
+    cv.textAlign = 'start'
+    if (Mode = 'N') {
+        cv.fillStyle = "#F00"
+        cv.fillText('×', 330, 190);
+    } else if (Mode = 'E') {
+        cv.fillStyle = "#FF0"
+        cv.fillText('△', 330, 190);
+    } else if (Mode = 'A') {
+        cv.fillStyle = "#0F0"
+        cv.fillText('〇', 330, 190);
+    } else if (Mode = 'N') {
+        cv.fillStyle = "#0F0"
+        cv.fillText('◎', 330, 190);
+    } else {
+        cv.fillStyle = "#0F0"
+        cv.fillText(Mode, 330, 190, 30);
+    }
+    
+    //速度計小数点
+    cv.fillStyle = "#FFF"
+    cv.beginPath();
+    cv.arc(300, 147, 4, 0, 3.142 * 2, true);
+    cv.closePath();
+    cv.fill();
+}
